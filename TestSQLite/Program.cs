c9d145c2f4f5407c7a5e28c12ef34ace803da026
gonzaonisimchuk <gonzaonisimchuk@hotmail.com>
2017-05-29 22:24:49 -0300

    first commit

---
+++ TestSQLite/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace TestSQLite
+{
+    using System;
+    using Microsoft.Data.Sqlite;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new SqliteConnection("Filename=sqlite.db");
+            db.Open();
+            var tableCommand = "CREATE TABLE IF NOT EXISTS Personas (Id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre NVARCHAR(200) NULL)";
+            var createTable = new SqliteCommand(tableCommand, db);
+            createTable.ExecuteReader();
+
+            //var command = new SqliteCommand("INSERT INTO Personas VALUES (NULL, @Entry)", db);
+            var command = new SqliteCommand("Update Personas set Nombre = @Entry Where Id = 4", db);
+            //Use parameterized query to prevent SQL injection attacks
+            Console.WriteLine("Nombre:");
+            var nombre = Console.ReadLine();
+            command.Parameters.AddWithValue("@Entry", nombre);
+            command.ExecuteReader();
+
+            Console.WriteLine("Nombres registrados:");
+            var query = new SqliteCommand("SELECT * from Personas", db).ExecuteReader();
+            while (query.Read())
+            {
+                Console.WriteLine($"{query.GetInt32(0)}, {query.GetString(1)}");
+            }
+            Console.ReadLine();
+            db.Close();
+        }
+    }
+}
